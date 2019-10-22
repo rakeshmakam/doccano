eba57f32e820a0d0aeb6b4d039a7050a4b3874e5
Rohan Jariwala <rohanj1607@gmail.com>
2019-10-22 13:26:28 +0530

    changes required

--- app/server/static/components/projects.vue
+++ app/server/static/components/projects.vue
@@ -111,7 +111,7 @@
                           div.dataset-item__main.is-vertical
                             div.dataset-item__main-title
                               div.dataset-item__main-title-link.dataset-item__link
-                                a.has-text-black(v-bind:href="'/projects/' + project.id")
+                                a.has-text-black(v-bind:href="'/annotate/projects/' + project.id")
                                   | {{ project.name }}
 
                             div.dataset-item__main-subtitle {{ project.description }}
@@ -123,7 +123,7 @@
                           span.tag.is-normal {{ project.project_type }}
 
                         td.is-vertical(v-if="isSuperuser")
-                          a(v-bind:href="'/projects/' + project.id + '/docs'") Edit
+                          a(v-bind:href="'/annotate/projects/' + project.id + '/docs'") Edit
 
                         td.is-vertical(v-if="isSuperuser")
                           a.has-text-danger(v-on:click="setProject(project)") Delete
